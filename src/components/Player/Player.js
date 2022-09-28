cba08d9bf28b28fa00f99fbf665d74a6100da7e0
unknown <mosharafctg111@gmail.com>
2022-09-29 02:32:12 +0600

    button setup

--- src/components/Player/Player.js
+++ src/components/Player/Player.js
@@ -4,12 +4,17 @@ const Player = (props) => {
     console.log(props)
     const {img,name, description ,Running,time}=props.game
     return (
-        <div className='run-img'>
+        <div className='player-card'>
             <img src={img} alt="" />
+            <div className="text-info">
             <h3> {name}</h3>
             <p><small>Description: {description}</small></p>
-            <p>Running Times:  {Running}</p>
+            <p><small>Running Times:  {Running}</small></p>
             <p>Time Required: {time} minutes</p>
+            </div>
+            <button className='btn-list'>
+                <p>Add to List</p>
+            </button>
         </div>
     );
 };
